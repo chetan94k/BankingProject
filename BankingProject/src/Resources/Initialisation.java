bdabf7a1fdc2633b6cf42262d6479e44eb46e58d
Chetan Sharma <chetan@sharma.git>
2018-03-04 19:18:05 +0530

    Description added

--- BankingProject/src/Resources/Initialisation.java
+++ BankingProject/src/Resources/Initialisation.java
@@ -36,7 +36,7 @@ public class Initialisation {
 			System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 			 driver=new ChromeDriver();
 			driver.get(geturl);
-			
+			//
 		}
 		else
 		{
